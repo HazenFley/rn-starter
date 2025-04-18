5be80ba3995742659d148a84271f54b804bc42df
HazenFley <chance.hoskins@growthheroes.com>
2025-04-18 11:53:46 -0500

    Basic styling

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>Component Screen</Text>;
+    const greeting = 'Konnichi wa';
+    const arrayNumbers = [1, 2, 3, 4, 5, 6];
+    const jsObjectsFail = { color: 'blue' }; //this does not work, no JS objects
+    const jsxTextElementExample = <Text style={styles.secondaryTextStyling}>This allows us to pass JSX objects!</Text>;
+
+    // returning all of this inside parens allows multi line statements
+    // Alternatively we could remove the parens, but the first <View> must be on the same line, no break
+    return (
+        <View>
+            <Text style={styles.textStyle}>Component Screen</Text>
+            <Text>Some additional text goes here, just without styling</Text>
+            <Text style={styles.secondaryTextStyling}>{greeting}</Text>
+            <Text style={styles.secondaryTextStyling}>{arrayNumbers}</Text>
+            {jsxTextElementExample}
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 30
+    },
+    secondaryTextStyling: {
+        fontSize: 20
     }
 });
 
